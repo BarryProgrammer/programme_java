3046ff93a7129e314e5fe9d0cd520633eae77266
Alimou Barry <alimoubarry4@gmail.com>
2025-09-18 02:15:53 +0000

    Ajout de Methode

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Bonjour les amies ");
+        System.out.println(x:"Bonjour les amies ");
+
+        int num = 5;
+        System.out.println(isUpperIs10(num));
     } 
+
+    public static boolean isUpperIs10(int num){
+        return num > 10;
+    }
 }
